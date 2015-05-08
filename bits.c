7de7cc0aa26c2977dfdeb82a53c017344bac8839
cpiper <cpiper@users.noreply.github.com>
2015-05-08 08:55:31 -0700

    Update bits.c

--- bits.c
+++ bits.c
@@ -1,11 +1,11 @@
 unsigned int most_signifcant(unsigned int num) {
-	unsigned int mask = ~0, i = sizeof(mask) << 2, bit = 0;
+	i = sizeof(mask) << 2, bit = 0;
 	if (!num)
     		return 0;
 	for (;i > 0; i >>= 1) {
 		if (num >> i) {
 			num >>= i;
-			ret += i;
+			bit += i;
 		}
 	}
 	return ++ret;
