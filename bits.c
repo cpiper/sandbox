a722fe0ad72294444208ce3c519efee86543972b
cpiper <cpiper@users.noreply.github.com>
2015-05-07 23:38:18 -0700

    Create bits.c

---
+++ bits.c
@@ -0,0 +1,14 @@
+unsigned int most_signifcant(unsigned int num) {
+	unsigned int mask = ~0, i = 16, bit = 0;
+	if (!num)
+    	return 0;
+	for (;i > 0; i >>= 1) {
+		if (num & mask << i) {
+			mask = (mask << i) & mask;
+			bit += i;
+		} else {
+			mask = (mask >> i) & mask;
+		}
+	}
+	return bit + 1;
+}
